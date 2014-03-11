22600bc4a94546901cc7c43fb9b2ba87ffd291b8
zhou <zhouworker@gmail.com>
2014-03-11 19:02:50 +0800

    解决null值处理的bug

--- src/org/zson/Json.java
+++ src/org/zson/Json.java
@@ -31,7 +31,7 @@ public class Json{
   }
   
   public static Json from(String json){
-    return new JsonReader(json).parse();
+    return json==null?new Json():new JsonReader(json).parse();
   }
 
   public boolean isArray(){
